674761342373620180b46354753c26cb813e3b99
Emmanuel <eboidot3@gatech.edu>
2015-06-11 04:10:22 -0700

    path as d3 element

--- example.py
+++ example.py
@@ -2,67 +2,8 @@
 import matplotlib.pyplot as plt
 import matplotlib.path as mpath
 import matplotlib.patches as mpatches
-import matplotlib as mpl
 import numpy as np
-
-from mpld3 import plugins,utils
 import pyLeaflet
-
-class DragPlugin(plugins.PluginBase):
-    JAVASCRIPT = r"""
-    mpld3.register_plugin("drag", DragPlugin);
-    DragPlugin.prototype = Object.create(mpld3.Plugin.prototype);
-    DragPlugin.prototype.constructor = DragPlugin;
-    DragPlugin.prototype.requiredProps = ["id"];
-    DragPlugin.prototype.defaultProps = {}
-    function DragPlugin(fig, props){
-        mpld3.Plugin.call(this, fig, props);
-        mpld3.insert_css("#" + fig.figid + " path.dragging",
-                         {"fill-opacity": "1.0 !important",
-                          "stroke-opacity": "1.0 !important"});
-    };
-
-    DragPlugin.prototype.draw = function(){
-        var obj = mpld3.get_element(this.props.id);
-
-        var drag = d3.behavior.drag()
-            .origin(function(d) { return {x:obj.ax.x(d[0]),
-                                          y:obj.ax.y(d[1])}; })
-            .on("dragstart", dragstarted)
-            .on("drag", dragged)
-            .on("dragend", dragended);
-
-        obj.elements()
-           .data(obj.offsets)
-           .style("cursor", "default")
-           .call(drag);
-
-        function dragstarted(d) {
-          d3.event.sourceEvent.stopPropagation();
-          d3.select(this).classed("dragging", true);
-        }
-
-        function dragged(d, i) {
-          d[0] = obj.ax.x.invert(d3.event.x);
-          d[1] = obj.ax.y.invert(d3.event.y);
-          d3.select(this)
-            .attr("transform", "translate(" + [d3.event.x,d3.event.y] + ")");
-        }
-
-        function dragended(d) {
-          d3.select(this).classed("dragging", false);
-        }
-    }
-    """
-
-    def __init__(self, points):
-        if isinstance(points, mpl.lines.Line2D):
-            suffix = "pts"
-        else:
-            suffix = None
-
-        self.dict_ = {"type": "drag",
-                      "id": utils.get_id(points, suffix)}
 
 fig, ax = plt.subplots()
 
@@ -94,7 +35,6 @@ line2 = ax.plot(x2, y2, 'ro', ms=5)
 ax.plot(x2, y2, '-g', ms=10)
 ax.axis('equal')
 
-plugins.connect(fig, DragPlugin(points[0]))
 ###############
 ###############
 # tile_layer = "http://{s}.tile.stamen.com/terrain/{z}/{x}/{y}.jpg"
